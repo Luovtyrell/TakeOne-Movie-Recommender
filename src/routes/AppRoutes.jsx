735f9ae94b71a51904bf175602e747c70da89099
Luovtyrell <luciakjhgfds@gmail.com>
2025-05-24 10:06:04 +0200

    Fix import statement for Home component to remove file extension

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/home/Home.jsx";
+import Home from "../pages/home/Home";
 import ResultsPage from "../pages/ResultsPage/ResultsPage";
 import MovieDetail from "../components/MovieDetails/MovieDetail";
 
