29524642af062469387dd021b7fbc647c0a6c5f1
Luovtyrell <luciakjhgfds@gmail.com>
2025-05-24 10:02:32 +0200

    Fix import statement for Home component to include file extension

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home/Home.jsx";
 import ResultsPage from "../pages/ResultsPage/ResultsPage";
 import MovieDetail from "../components/MovieDetails/MovieDetail";
 
